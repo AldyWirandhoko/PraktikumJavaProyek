de4da54dc1ad08180bb4cd95e7073ab0e2507425
Aldy Wirandhoko <103399203+AldyWirandhoko@users.noreply.github.com>
2022-07-02 07:36:28 +0800

    Update pada KabupatenInputFrame

--- src/main/java/frame/KabupatenInputFrame.java
+++ src/main/java/frame/KabupatenInputFrame.java
@@ -5,6 +5,7 @@ import helpers.Koneksi;
 import javax.swing.*;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class KabupatenInputFrame extends JFrame{
@@ -18,11 +19,20 @@ public class KabupatenInputFrame extends JFrame{
             Connection c = Koneksi.getConnection();
             PreparedStatement ps;
             try {
-                String insertSQL = "INSERT INTO kabupaten VALUES (NULL, ?)";
-                ps = c.prepareStatement(insertSQL);
-                ps.setString(1, nama);
-                ps.executeUpdate();
-                dispose();
+                if (id == 0) {
+                    String insertSQL = "INSERT INTO kabupaten VALUES (NULL, ?)";
+                    ps = c.prepareStatement(insertSQL);
+                    ps.setString(1, nama);
+                    ps.executeUpdate();
+                    dispose();
+                } else {
+                    String updateSQL = "UPDATE kabupaten SET nama = ? WHERE id = ?";
+                    ps = c.prepareStatement(updateSQL);
+                    ps.setString(1, nama);
+                    ps.setInt(2, id);
+                    ps.executeUpdate();
+                    dispose();
+                }
             } catch (SQLException ex) {
                 throw new RuntimeException(ex);
             }
@@ -37,10 +47,32 @@ public class KabupatenInputFrame extends JFrame{
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setLocationRelativeTo(null);
     }
+
+    public void isiKomponen() {
+        Connection c = Koneksi.getConnection();
+        String findSQL = "SELECT * FROM kabupaten WHERE id = ?";
+        PreparedStatement ps = null;
+        try {
+            ps = c.prepareStatement(findSQL);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                idTextField.setText(String.valueOf(rs.getInt("id")));
+                namaTextField.setText(rs.getString("nama"));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
     private JPanel mainPanel;
     private JTextField idTextField;
     private JTextField namaTextField;
     private JPanel buttonPanel;
     private JButton batalButton;
+
+    private int id;
+    public void setId(int id) {
+        this.id = id;
+    }
     private JButton simpanButton;
 }
--- src/main/java/frame/KabupatenViewFrame.java
+++ src/main/java/frame/KabupatenViewFrame.java
@@ -73,6 +73,21 @@ public class KabupatenViewFrame extends JFrame{
                 }
             }
         });
+        ubahButton.addActionListener(e ->{
+            int barisTerpilih = viewTable.getSelectedRow();
+            if(barisTerpilih < 0){
+                JOptionPane.showMessageDialog(
+                        null,
+                        "Pilih data dulu");
+                return;
+            }
+            TableModel tm = viewTable.getModel();
+            int id = Integer.parseInt(tm.getValueAt(barisTerpilih,0 ).toString());
+            KabupatenInputFrame inputFrame = new KabupatenInputFrame();
+            inputFrame.setId(id);
+            inputFrame.isiKomponen();
+            inputFrame.setVisible(true);
+        });
         isiTable();
         init();
     }
