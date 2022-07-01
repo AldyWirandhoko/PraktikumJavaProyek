0ccbcdb8c6980214adc00caa1a510b3512fed207
Aldy Wirandhoko <103399203+AldyWirandhoko@users.noreply.github.com>
2022-07-01 15:34:14 +0800

    Membuat KabupatenViewFrame

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,10 @@
+import frame.KabupatenViewFrame;
 import helpers.Koneksi;
 
 public class Main {
     public static void main(String[] args) {
-        Koneksi.getConnection();
+//        Koneksi.getConnection();
+        KabupatenViewFrame viewFrame = new KabupatenViewFrame();
+        viewFrame.setVisible(true);
     }
 }
---
+++ src/main/java/frame/KabupatenViewFrame.java
@@ -0,0 +1,33 @@
+package frame;
+
+import javax.swing.*;
+
+
+public class KabupatenViewFrame extends JFrame{
+    public KabupatenViewFrame(){
+        init();
+    }
+
+    private void init() {
+        setContentPane(mainPanel);
+        setTitle("Data Kabupaten");
+        pack();
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+    private JPanel mainPanel;
+    private JPanel cariPanel;
+    private JScrollPane viewScrollPane;
+    private JPanel buttonPanel;
+    private JTextField cariTextField;
+    private JButton cariButton;
+    private JTable viewTable;
+    private JButton tambahButton;
+    private JButton ubahButton;
+    private JButton hapusButton;
+    private JButton batalButton;
+    private JButton cetakButton;
+    private JButton tutupButton;
+}
+
+
