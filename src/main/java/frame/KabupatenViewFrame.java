02924f69929bd3ab8d6c688e1654ff6083ac47fb
Aldy Wirandhoko <103399203+AldyWirandhoko@users.noreply.github.com>
2022-07-02 03:28:54 +0800

    Read, Search, Delete pada KabupatenViewFrame

--- src/main/java/frame/KabupatenViewFrame.java
+++ src/main/java/frame/KabupatenViewFrame.java
@@ -1,10 +1,75 @@
 package frame;
 
+import helpers.Koneksi;
+
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.sql.*;
 
 
 public class KabupatenViewFrame extends JFrame{
     public KabupatenViewFrame(){
+        tutupButton.addActionListener(e -> {
+            dispose();
+        });
+        batalButton.addActionListener(e -> {
+            isiTable();
+        });
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowActivated(WindowEvent e) {
+                isiTable();
+            }
+        });
+        cariButton.addActionListener(e -> {
+            Connection c = Koneksi.getConnection();
+            String keyword = "%" + cariTextField.getText() + "%";
+            String searchSQl = "SELECT * FROM kabupaten WHERE nam like ?";
+            try {
+                PreparedStatement ps =c.prepareStatement(searchSQl);
+                ps.setString(1, keyword);
+                ResultSet rs = ps.executeQuery();
+                DefaultTableModel dtm =(DefaultTableModel) viewTable.getModel();
+                dtm.setRowCount(0);
+                Object[] row = new Object[2];
+                while (rs.next()){
+                    row[0] = rs.getInt("id");
+                    row[1] = rs.getString("nama");
+                    dtm.addRow(row);
+                }
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+        hapusButton.addActionListener(e -> {
+            int barisTerpilih = viewTable.getSelectedRow();
+            if(barisTerpilih < 0){
+                JOptionPane.showMessageDialog(null,"Pilih data dulu");
+                return;
+            }
+            int pilihan = JOptionPane.showConfirmDialog(null,
+                    "Yakin mau hapus?",
+                    "Konfirmasi Hapus",
+                    JOptionPane.YES_NO_CANCEL_OPTION
+            );
+            if(pilihan == 0){
+                TableModel tm = viewTable.getModel():
+                int id = Integer.parseInt(tm.getValueAt(barisTerpilih, 0).toString());
+                Connection c = Koneksi.getConnection();
+                String deleteSQL = "DELETE FROM kabupaten WHERE id = ?";
+                try {
+                    PreparedStatement ps = c.prepareStatement(deleteSQL);
+                    ps.setInt(1, id);
+                    ps.executeUpdate();
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+        isiTable();
         init();
     }
 
@@ -14,6 +79,25 @@ public class KabupatenViewFrame extends JFrame{
         pack();
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setLocationRelativeTo(null);
+    }
+    public void isiTable(){
+        Connection c = Koneksi.getConnection();
+        String selectSQL = "SELECT * FROM kabupaten";
+        try {
+            Statement s = c.createStatement();
+            ResultSet rs = s.executeQuery(selectSQL);
+            String header[] = {"Id", "Nama Kabupaten"};
+            DefaultTableModel dtm = new DefaultTableModel(header, 0);
+            viewTable.setModel(dtm);
+            Object[] row = new Object[2];
+            while (rs.next()){
+                row[0] = rs.getInt("id");
+                row[1] = rs.getString("nama");
+                dtm.addRow(row);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
     private JPanel mainPanel;
     private JPanel cariPanel;
